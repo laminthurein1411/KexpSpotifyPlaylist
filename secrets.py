b5a8782813602e5d0094245642c142532369357e
Spencer-Root <45667672+Spencer-Root@users.noreply.github.com>
2020-06-11 17:10:51 -0700

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,4 @@
+spotify_username = {your Spotify username}
+spotify_client_id = {your Spotify client ID}
+spotify_secret = {your Spotify developer key}
+redirect_url = "http://localhost:8888/callback/"
