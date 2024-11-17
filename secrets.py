9a658a23086c4448954165db6fdccaeda7b22ff0
laminthurein1411 <78501447+laminthurein1411@users.noreply.github.com>
2024-11-17 17:26:15 +0630

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,4 +1,4 @@
-spotify_username = cdl24hkuj7v7dj6n9l3lyi3j1
-spotify_client_id = 92c1158d047543aeb062d1cffc48ed03
-spotify_secret = c2f89b466cdf413087879d58911dc61a
+spotify_username = "cdl24hkuj7v7dj6n9l3lyi3j1"
+spotify_client_id = "92c1158d047543aeb062d1cffc48ed03"
+spotify_secret = "c2f89b466cdf413087879d58911dc61a"
 redirect_url = "http://localhost:8888/callback/"
